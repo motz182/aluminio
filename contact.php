5894b77118f777a0ba73193a64b6ee8d306d1ec7
rpercastro <rpercastro@gmail.com>
2016-10-15 17:33:46 +0100

    v20161014 - Se añade el backend del formulario de contacto (contact.php)

---
+++ contact.php
@@ -0,0 +1,22 @@
+<?php
+    $name = $_POST[name];
+    $email =$_POST[email];
+    $phone =$_POST[phone];
+    $message =$_POST[message];
+
+    $for =  "rpercastro@gmail.com";
+    $subject = "Mensaje - Contacto www.orionaluminios.com";
+    $content = "
+    Nombre: ".$name."
+    
+    E-mail: ".$email."
+    
+    Teléfono: ".$phone."
+    
+    Mensaje: ".$message."
+    ";
+
+    mail($for, $subject, utf8_decode($content));
+
+    header("location:index.html");
+?>
